0bc82e52b51df3471bec83b5b42312bf267a2b1d
Benhur Alencar Azevedo <benhur.azevedo@hotmail.com>
2023-06-23 07:57:28 -0300

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>O seu nome é: {{ nome }}</h2>
   </div>
 </template>
+<script>
+import api from '../services/api'
+export default {
+  name: 'about',
+  data: function(){
+    return {
+      nome: null
+    };
+  },
+  mounted(){
+    api.get('/getnome')
+      .then((response) => {
+        return response.data;
+      })
+      .then((data) =>{
+        this.nome = data.nome;
+      })
+  },
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          sm="6"
+          md="4"
+        >
+          <v-text-field
+            dense
+            label="Digite o nome"
+            v-model="nome"
+          ></v-text-field>
+          <v-btn
+            color="primary"
+            elevation="2"
+            @click="enviar"
+          >Envie</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  import api from '../services/api'
+  export default {
+    name: 'Home',
+    data: function(){
+      return {
+        nome: null
+      }
+    },
+    methods:{
+      enviar: function(){
+        if(this.nome == null)
+        {
+          alert("preencha o campo!");
+          return;
+        }
+        else if(this.nome.length == 0)
+        {
+          alert("preencha o campo!");
+          return;
+        }
+        else 
+        {
+          //console.log(JSON.stringify({nome: this.nome}));
+          api.post('/setnome',{nome: this.nome})
+            .then((response) => {
+              this.$router.push('/about');
+            });
+        }
+      },
+    },
   }
-}
 </script>
